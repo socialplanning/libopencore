1155df416769a27e0e25caf97a641005dec234e6
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-02-25 23:18:34 +0000

    mucking around with making links correct. this stuff is tricky.
    git-svn-id: https://svn.socialplanning.org/svn/opencore/libopencore/trunk@28065 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- libopencore/http_proxy.py
+++ libopencore/http_proxy.py
@@ -1,6 +1,6 @@
 from wsgifilter import proxyapp
 
-vhm_template = "VirtualHostBase/%(wsgi.url_scheme)s/%(HTTP_HOST)s:%(frontend_port)s"
+vhm_template = "/VirtualHostBase/%(wsgi.url_scheme)s/%(HTTP_HOST)s:%(frontend_port)s"
 
 def app_factory(global_conf,
                 remote_uri=None,
@@ -44,9 +44,9 @@ class RemoteProxy(object):
                  robots_uri=None, site_root=None):
         remote_uris = remote_uris or []
 
-        # make sure there's a trailing slash
+        # make sure there's no trailing slash
         self.remote_uris = [
-            remote_uri.rstrip('/') + '/' 
+            remote_uri.rstrip('/')
             for remote_uri in remote_uris
             ]
         
@@ -121,6 +121,95 @@ class RemoteProxy(object):
 
         # work around bug in WSGIFilter
         environ_copy = environ.copy()
-
         
-        return app(environ_copy, start_response)
+        from webob import Request
+        request = Request(environ_copy)
+        resp = request.get_response(app)
+
+        resp = rewrite_links(
+            Request(environ), resp,
+            url_normalize(remote_uri),
+            url_normalize(Request(environ).application_url),
+            url_normalize('%s://%s%s' % (
+                    request.scheme, 
+                    request.host,
+                    request.path_qs))
+            )
+        
+        return resp(environ, start_response)
+
+from deliverance.util.urlnormalize import url_normalize
+import re
+_cookie_domain_re = re.compile(r'(domain="?)([a-z0-9._-]*)("?)', re.I)
+from lxml.html import document_fromstring, tostring
+import urlparse
+
+def rewrite_links(request, response,
+                  proxied_base, orig_base,
+                  proxied_url):
+
+    exact_proxied_base = proxied_base
+    if not proxied_base.endswith('/'):
+        proxied_base += '/'
+    exact_orig_base = orig_base
+    if not orig_base.endswith('/'):
+        orig_base += '/'
+    assert (proxied_url.startswith(proxied_base) 
+            or proxied_url.split('?', 1)[0] == proxied_base[:-1]), (
+        "Unexpected proxied_url %r, doesn't start with proxied_base %r"
+        % (proxied_url, proxied_base))
+    assert (request.url.startswith(orig_base) 
+            or request.url.split('?', 1)[0] == orig_base[:-1]), (
+        "Unexpected request.url %r, doesn't start with orig_base %r"
+        % (request.url, orig_base))
+
+    def link_repl_func(link):
+        """Rewrites a link to point to this proxy"""
+        if link == exact_proxied_base:
+            return exact_orig_base
+        if not link.startswith(proxied_base):
+            # External link, so we don't rewrite it
+            return link
+        new = orig_base + link[len(proxied_base):]
+        return new
+    if response.content_type != 'text/html':
+        pass
+    else:
+        if not response.charset:
+            ## FIXME: maybe we should guess the encoding?
+            body = response.body
+        else:
+            body = response.unicode_body
+        body_doc = document_fromstring(body, base_url=proxied_url)
+        body_doc.make_links_absolute()
+        body_doc.rewrite_links(link_repl_func)
+        response.body = tostring(body_doc)
+
+    if response.location:
+        ## FIXME: if you give a proxy like
+        ## http://openplans.org, and it redirects to
+        ## http://www.openplans.org, it won't be rewritten and
+        ## that can be confusing -- it *shouldn't* be
+        ## rewritten, but some better log message is required
+        loc = urlparse.urljoin(proxied_url, response.location)
+        loc = link_repl_func(loc)
+        response.location = loc
+        
+    if 'set-cookie' in response.headers:
+        cookies = response.headers.getall('set-cookie')
+        del response.headers['set-cookie']
+        for cook in cookies:
+            old_domain = urlparse.urlsplit(proxied_url)[1].lower()
+            new_domain = request.host.split(':', 1)[0].lower()
+            def rewrite_domain(match):
+                """Rewrites domains to point to this proxy"""
+                domain = match.group(2)
+                if domain == old_domain:
+                    ## FIXME: doesn't catch wildcards and the sort
+                    return match.group(1) + new_domain + match.group(3)
+                else:
+                    return match.group(0)
+            cook = _cookie_domain_re.sub(rewrite_domain, cook)
+            response.headers.add('set-cookie', cook)
+
+    return response
--- libopencore/wsgi.py
+++ libopencore/wsgi.py
@@ -141,6 +141,8 @@ class URLDispatcher(object):
                 environ['HTTP_X_OPENPLANS_APPLICATION'] = 'zope'
             return self.default_app(environ, start_response)
 
+        #print new_script_name, new_path_info
+
         # this is pretty hideous but i don't have time to find the source
         # of the bug.  if wordpress receives a PATH_INFO without a leading
         # slash it constructs URLs missing that slash like /myproject/blogwpadmin.php
@@ -148,11 +150,11 @@ class URLDispatcher(object):
         # it loses all the controller routing information from PATH_INFO internally
         # so a URL like /myproject/tasks/tasklist/show_create is treated as if 
         # it were /myproject/tasks/show_create and results in a 404.
-        new_path_info = new_path_info.lstrip('/')
-        if app_name not in ("tasktracker", "zine") and new_path_info:
-            new_path_info = "/%s" % new_path_info
+        #new_path_info = new_path_info.lstrip('/')
+        #if app_name not in ("tasktracker", "zine") and new_path_info:
+        #    new_path_info = "/%s" % new_path_info
 
-        new_script_name = new_script_name.rstrip('/')
+        #new_script_name = new_script_name.rstrip('/')
 
         environ['PATH_INFO'], environ['SCRIPT_NAME'] = (
             new_path_info, new_script_name)
